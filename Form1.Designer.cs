f1ce324f86341c6d2c2aede520994f1e17d8d702
SkylerD2F <Bcolli7606@my.sullivan.edu>
2022-06-05 13:37:51 -0400

    restarted project

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -113,7 +113,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Rock Paper Scissors";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
